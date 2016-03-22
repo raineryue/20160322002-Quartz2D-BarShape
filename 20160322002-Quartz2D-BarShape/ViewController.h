957f0aa637002142f8923f0bdf040ee5c7bea7ed
raineryue <jinlei_yue@sina.com>
2016-03-22 16:17:25 +0800

    Initial Commit

---
+++ 20160322002-Quartz2D-BarShape/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160322002-Quartz2D-BarShape
+//
+//  Created by Rainer on 16/3/22.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
